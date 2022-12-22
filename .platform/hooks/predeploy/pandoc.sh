06357d2443f942f1c6fffa18a25f03daddd10763
densumesh <dens.sumesh79@gmail.com>
2022-12-22 11:43:12 -0700

    less resources

--- .platform/hooks/predeploy/pandoc.sh
+++ .platform/hooks/predeploy/pandoc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://github.com/jgm/pandoc/releases/download/2.19.2/pandoc-2.19.2-linux-amd64.tar.gz
-tar xvzf pandoc-2.19.2-linux-amd64.tar.gz --strip-components 1 -C /usr/local
+# wget https://github.com/jgm/pandoc/releases/download/2.19.2/pandoc-2.19.2-linux-amd64.tar.gz
+# tar xvzf pandoc-2.19.2-linux-amd64.tar.gz --strip-components 1 -C /usr/local
